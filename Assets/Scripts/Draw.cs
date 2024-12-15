4592585ea402f938b660b7d6479f84ae11619082
ChristopheCai <caichristo@cy-tech.fr>
2024-12-15 23:16:00 +0100

    Rework UI

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -1,43 +1,46 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Draw : MonoBehaviour
 {
-    private LineRenderer lineRenderer;
-    private Vector3 previousPosition;
-    
+    [SerializeField] private GameObject linePrefab;
     [SerializeField] private float minDistance = 0.1f;
 
-    private void Start()
-    {
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 1;
-        previousPosition = transform.position;
-    }
+    private LineRenderer currentLineRenderer;
+    private Vector3 previousPosition;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0) && Camera.main is not null)
+        if (Input.GetMouseButtonDown(0))
         {
-            var currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentPosition.z = 0.0f;
+            StartNewLine();
+        }
 
-            if (Vector3.Distance(currentPosition, previousPosition) > minDistance)
-            {
-                
-                if (previousPosition == transform.position)
-                {
-                    lineRenderer.SetPosition(0, currentPosition);
-                }
-                else
-                {
-                    lineRenderer.positionCount++;
-                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, currentPosition);
-                }
-                previousPosition = currentPosition;
-            }
+        if (Input.GetMouseButton(0) && currentLineRenderer != null)
+        {
+            DrawLine();
+        }
+    }
+
+    private void StartNewLine()
+    {
+        GameObject newLine = Instantiate(linePrefab, transform);
+        currentLineRenderer = newLine.GetComponent<LineRenderer>();
+
+        previousPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        previousPosition.z = 0.0f;
+    }
+
+    private void DrawLine()
+    {
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentPosition.z = 0.0f;
+
+        if (Vector3.Distance(currentPosition, previousPosition) > minDistance)
+        {
+            currentLineRenderer.positionCount++;
+            currentLineRenderer.SetPosition(currentLineRenderer.positionCount - 1, currentPosition);
+            previousPosition = currentPosition;
         }
     }
 }
